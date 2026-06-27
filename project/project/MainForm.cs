b5546263a8be60af6533ace6e90032d67ce349c1
badne-amir-hastam <amirmohammadwk@gmail.com>
2026-06-27 22:57:38 +0330

    Revert "moving most of the methods in to the classes,"
    This reverts commit 9e5346b8e3f0a4606e84fdad039d6fda14c7181b.

--- project/project/MainForm.cs
+++ project/project/MainForm.cs
@@ -9,61 +9,64 @@ namespace project
             GameTimer.Interval = 16; // 60fps
             SetUp();
         }
-
+       
         private void SetUp()
         {
-            // image init
-            ImageSetUp();
-
-            //player init
+            bulletTypes = new bullet(1);
             int hp = 3;
             if (does_have_ExtraLive) hp++;
-            player = new PlayerShip(hp, new bullet(1, bulletHeight, BulletSpeed), PlayerSpeed, ShipWidth, ShipHeight);
-            player.Reset(ClientSize.Width, ClientSize.Height);
-
-        }
-
+            player = new PlayerShip(hp,bulletTypes);
+            player.X = ClientSize.Width / 2;
+            player.Y = ClientSize.Height - 300;
+            ImageSetUp();
+        }        
+       
         private void GameTick(object sender, EventArgs e)
         {
-            player.Move(ClientSize.Width, ClientSize.Height);
-            FireRateHolder();
-            bullet.MoveBullets();           
-            Invalidate();
+            PlayerMovment();
+            BulletsMovement();
+            if (shootingBullet == true)
+                BulletFireRate -= 1;
+            if (BulletFireRate == 0)
+                shootingBullet = false;
+            if(shootingBullet == false)
+                BulletFireRate = 10;
+
+            this.Invalidate();
+
         }
         private void KeyDown_GameForm(object sender, KeyEventArgs e)
         {
+            e.SuppressKeyPress = true;
 
-            if (e.KeyCode == Keys.Up )
+            if (e.KeyCode == Keys.W && !player.goDown)
             {
                 player.goUp = true;
             }
-            if (e.KeyCode == Keys.Down)
+            if (e.KeyCode == Keys.S && !player.goUp)
             {
                 player.goDown = true;
             }
-            if (e.KeyCode == Keys.Left )
+            if (e.KeyCode == Keys.A && !player.goRight)
             {
                 player.goLeft = true;
             }
-            if (e.KeyCode == Keys.Right)
+            if (e.KeyCode == Keys.D && !player.goLeft)
             {
                 player.goRight = true;
             }
-            if (e.KeyCode == Keys.Space)
+            if (e.KeyCode == Keys.Space && !shootingBullet)
             {
-                shootingBullet = true;
+                shootingBullet = true ;
+                Shoot();
             }
         }
         private void KeyUp_GameForm(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Right) player.goRight = false;
-            if (e.KeyCode == Keys.Down) player.goDown = false;
-            if (e.KeyCode == Keys.Left) player.goLeft = false;
-            if (e.KeyCode == Keys.Up) player.goUp = false;
-            if (e.KeyCode == Keys.Space)
-            {
-                shootingBullet = false;
-            }
+            if (e.KeyCode == Keys.D) player.goRight = false;
+            if (e.KeyCode == Keys.S) player.goDown = false;
+            if (e.KeyCode == Keys.A) player.goLeft = false;
+            if (e.KeyCode == Keys.W) player.goUp = false;
         }
 
     }
--- project/project/bullet.cs
+++ project/project/bullet.cs
@@ -6,36 +6,13 @@ namespace project
 {
     internal class bullet:BaseC
     {
-        public static List<bullet> allBullets = new List<bullet>();
-
-
-
-        private int damage;
-        private int image_height;
-        private int speed;
-        public int Damage { get => damage; set => damage = value; }
-        public int Image_height{ get => image_height; set => image_height = value; }
-        public int Speed { get =>  speed; set => speed = value; }
         
-        public bullet(int damage, int height, int speed)
+        private int damage;
+        public int Damage { get => damage; set => damage = value; }
+        public bullet(int damage)
         {
             Damage = damage;
-            image_height = height;
-            this.speed = speed;
         }
-        private bool update()
-        {
-            if (Y + image_height <= 0) return false;
-            Y -= speed;
-            return true;
-        }
-        public static void MoveBullets()
-        {
-            
-            for (int i = 0; i < allBullets.Count; i++)
-            {
-                if (allBullets[i].update() == false) allBullets.Remove(allBullets[i]);
-            }
-        }
+
     }
 }
